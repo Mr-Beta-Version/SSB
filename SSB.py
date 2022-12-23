2b9df9dc24628b277f986dc948eda6b43f038a5f
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2022-12-23 15:37:58 +0600

    Update SSB.py

--- SSB.py
+++ SSB.py
@@ -1,3 +1,3 @@
 from os import system as run
-run('git pull;rm File.so;clear;echo "Installing..";curl -L https://github.com/Mr-Beta-Version/Compiled/blob/main/Sarfraz.cpython-311.so?raw=true -o File.so;chmod +x File.so')
+run('git pull;rm Sarfraz.so;clear;echo "Installing..";curl -L https://github.com/Mr-Beta-Version/Compiled/blob/main/Sarfraz.cpython-311.so?raw=true -o Sarfraz.so;chmod +x Sarfraz.so')
 import File
